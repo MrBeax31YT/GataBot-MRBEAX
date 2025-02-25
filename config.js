b2872705397f339969cb4ea070bf73595fd6dff7
MrBeax31YT <139056445+MrBeax31YT@users.noreply.github.com>
2025-02-25 19:27:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,17 +13,8 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["5214774444444", '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿
-["5492266613038"], 
-["5573031859"],
-["5218715746374"],
-["593968585383"],
-["595976126756"],
-["584125778026"],
-["593968263524"],
-["5215539356057"],
-["573012482597"],
-["51968374620"]]
+["34668548317", 'Owner', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿
+]
 
 global.mods = []
 global.prems = []
